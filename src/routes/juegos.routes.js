080f5754c03f87532e2bcf931c36a7e1a641e99f
zaxwtf <zaxwtf06@gmail.com>
2026-06-02 21:05:02 +0200

    refactor: borradas variables innecesarias y cambiado nombre de otras

--- src/routes/juegos.routes.js
+++ src/routes/juegos.routes.js
@@ -1,15 +1,14 @@
 const { Router } = require('express');
 const router = Router();
 const juegosController = require('../controllers/juegos.controller');
-const validarJuego = require('../middlewares/validarJuego')
 const {body, validationResult} = require('express-validator')
-const test = require('../middlewares/validar')
+const validate = require('../middlewares/validar')
 
 
 //Definimos rutas de get y post y enlazamos a la función del controller
 router.get('/', juegosController.obtenerJuegos);
 router.get('/:id', juegosController.obtenerJuegoID)
-router.post('/crear', validarJuego.validarJuego, test.validate, juegosController.crearJuego);
+router.post('/crear', validate.validarJuego, validate.validate, juegosController.crearJuego);
 router.delete('/borrar/:id', juegosController.borrarJuego)
 router.put('/cambiar/:id', juegosController.actualizarJuego);
 
