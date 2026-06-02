22d178724d82aaa19c40a8f3b48bf0d40654f8fd
zaxwtf <zaxwtf06@gmail.com>
2026-06-02 22:22:30 +0200

    feat: indicada ruta de validación de users en users.routes

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -2,11 +2,12 @@ const { Router } = require('express')
 const router = Router()
 const usuariosController = require('../controllers/usuarios.controller');
 const validarUsuario = require('../middlewares/validarUser');
+const validate = require('../middlewares/validar')
 
 
 //Rutas usuarios
 
-router.post('/crear', usuariosController.crearUser);
+router.post('/crear', validate.validarUser, validate.validate, usuariosController.crearUser);
 router.get('/email', usuariosController.obtenerUserByEmail)
 router.get('/:id', usuariosController.obtenerUserById)
 
