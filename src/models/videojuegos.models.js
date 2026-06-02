5cdc13503f470e7702bbe2c4ad18aecde466fc5a
zaxwtf <zaxwtf06@gmail.com>
2026-06-02 15:04:58 +0200

    refactor: modificado sistema para tratar los datos para que funcione con base de datos mongo

--- src/models/videojuegos.models.js
+++ src/models/videojuegos.models.js
@@ -1,62 +1,47 @@
-const fs = require('fs').promises
-const path = require('path')
+const mongoose = require('mongoose');
 
-const RUTA = path.join(__dirname, "videojuegos.json")
-
-async function leerVideojuegos() {
-    try{
-        const contenido = await fs.readFile(RUTA, "utf-8")
-        return JSON.parse(contenido)
-    } catch(error){
-        if (error.code === "ENOENT"){
-            await fs.writeFile(RUTA, "[]", "utf-8")
-            return []
-        }
-        throw error
+const gameSchema = new mongoose.Schema(
+    {
+        nombre: {
+            type: String || Number,
+            required: true,
+            trim: true,
+        },
+        precio: {
+            type: Number,
+            required: true,
+        },
+    },
+    {
+        timestamps: true
     }
-}
+);
 
-async function escribirVideojuegos(videojuegos) {
-    await fs.writeFile(RUTA, JSON.stringify(videojuegos, null, 2), "utf-8")
-}
+const Videogame = mongoose.model("Videogame", gameSchema)
 
 
 async function getAllVideogames() {
-    return await leerVideojuegos()
+    return await Videogame.find()
 }
 
 async function getVideojuegosById(id) {
-    const videojuegos = await leerVideojuegos()
-    return videojuegos.find(juego => juego.id === id) || null;
+    return await Videogame.findById(id)
 }
 
 async function crearJuego(data) {
-    const juegos = await leerVideojuegos()
-    const newId = juegos.length > 0 ? Math.max(...juegos.map(n => n.id)) + 1 : 1;
-    const newGame = {id: newId, ...data}
-    juegos.push(newGame)
-    await escribirVideojuegos(juegos)
-    return newGame
+    const newGame = new Videogame(data)
+    return await newGame.save()
 }
 
 async function updateJuegos(id, data) {
-    const juegos = await leerVideojuegos()
-    const indice = juegos.findIndex(p => p.id === id)
-    if (indice === -1) return null
-    juegos[indice] = {id, ...data}
-    await escribirVideojuegos(juegos)
-    return juegos[indice]
+    return await Videogame.findByIdAndUpdate(id, data, {
+        new: true,
+        runValidators: true,
+    })
 }
 
 async function deleteGame(id) {
-
-    const juegos = await leerVideojuegos()
-    const videojuegoFind = juegos.find(p => p.id === id)
-    if (videojuegoFind){
-        const NuevoVideojuegos = juegos.filter(p => p.id !== id )
-        const NuevoJSON = await escribirVideojuegos(NuevoVideojuegos)
-        return NuevoVideojuegos
-    }
+    return await Videogame.findByIdAndDelete(id)
 }
 
 
