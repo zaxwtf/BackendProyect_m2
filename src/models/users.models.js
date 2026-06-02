a361c04ee8fbd85be598dd54aca12dcf36efaedd
zaxwtf <zaxwtf06@gmail.com>
2026-06-02 22:18:14 +0200

    refactor: agregado userNameUnico a esquema de User para mongoose

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -18,10 +18,14 @@ const userSchema = new mongoose.Schema(
             type: String,
             required: true,
         },
+        userNameUnico: {
+            type: String,
+            required: true,
+        },
         email: {
             type: String,
             required: true,
-        }
+        },
     },
     {
         timestamps: true
