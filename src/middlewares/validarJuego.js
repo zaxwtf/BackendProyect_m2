36c0028596e26b9b9f55d064daebb6929477591e
zaxwtf <zaxwtf06@gmail.com>
2026-06-02 16:57:24 +0200

    refactor: agregada lista de generos de juegos y agregados al comprobador para ver si lo contiene y coincide con los de la lista

--- src/middlewares/validarJuego.js
+++ src/middlewares/validarJuego.js
@@ -1,7 +1,21 @@
+const genders = [
+    "lucha",
+    "rpg",
+    "arcade",
+    "plataformas",
+    "disparos",
+    "simulacion",
+    "estrategia",
+    "a_rpg"
+]
+
+
 const validarProducto = (req, res, next) =>{
-    const {nombre, precio} = req.body;
-    if (!nombre || !precio){
-        return res.status(400).json({error: "El juego debe tener nombre y precio"})
+    const {nombre, precio, genero} = req.body;
+    if (!nombre || !precio || !genero){
+        return res.status(404).json({error: "El juego debe tener nombre, precio y género"})
+    } else if (!(genders.includes(genero))){
+        return res.status(404).json({error: "El juego debe tener un género valido"})
     }
 
     next()
