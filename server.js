0bfa9c6aa176f679a3d4fc7c383549444ddb9db4
zaxwtf <zaxwtf06@gmail.com>
2026-06-02 15:06:40 +0200

    refactor: cambiado inicio de server para que se conecte con mongoDB

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
+require("dotenv").config()
 const app = require('./src/app');
+const connectDB = require('./src/config/db')
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=>{
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: "ok" });
 });
 
-app.get('/health', (req, res) =>{
-    res.status(200).json({status: ok})
-})
+
+
+async function initServer() {
+    await connectDB();
+    app.listen(PORT, () => {
+        console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    });
+}
+
+initServer();
