4c5257f340fe9646a2c956e6af78cf691950f3cb
zaxwtf <zaxwtf06@gmail.com>
2026-06-02 16:58:07 +0200

    feat: creado esquema para usuarios en mongo y funciones para tratar los datos de la DB para CRUD

---
+++ src/models/users.models.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+    {
+        nombre: {
+            type: String,
+            required: true,
+        },
+        apellido1: {
+            type: String,
+            required: true,
+        },
+        apellido2: {
+            type: String,
+            required: false,
+        },
+        userName: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+const User = mongoose.model("User", userSchema)
+
+
+async function getUsersById(id) {
+    return await User.findById(id)
+}
+
+async function getUsersByEmail(email) {
+    return await User.find(User => User.email === email)
+}
+
+async function crearUser(data) {
+    const newUser = new User(data)
+    return await newUser.save()
+}
+
+async function updateUser(id, data) {
+    return await User.findByIdAndUpdate(id, data, {
+        new: true,
+        runValidators: true,
+    })
+}
+
+async function deleteUser(id) {
+    return await User.findByIdAndDelete(id)
+}
+
+
+module.exports = {
+    getUsersById,
+    getUsersByEmail,
+    crearUser,
+    updateUser,
+    deleteUser
+}
