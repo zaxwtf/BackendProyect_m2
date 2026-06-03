0984707773e8fef3c871a133ad8bdd795dd28f06
zaxwtf <zaxwtf06@gmail.com>
2026-06-03 19:49:56 +0200

    refactor: agregado limite de caracteres a todos los campos para evitar campos de texto demasiado largos

--- src/middlewares/validarUser.js
+++ src/middlewares/validarUser.js
@@ -3,13 +3,13 @@ const { body, validationResult } = require('express-validator')
 
 const validarUser = [
     body('nombre')
-        .notEmpty().withMessage('El usuario debe tener nombre'),
+        .isLength({min: 2, max: 40}).withMessage('El usuario debe tener nombre'),
     body('apellido1')
-        .notEmpty().withMessage('El usuario debe tener por lo menos el primer apellido'),
+        .isLength({min: 2, max: 40}).withMessage('El usuario debe tener por lo menos el primer apellido'),
     body('userName')
-        .notEmpty().withMessage('El usuario debe tener nombre de usuario'),
+        .isLength({min: 2, max: 40}).withMessage('El usuario debe tener nombre de usuario'),
     body('email')
-        .notEmpty().isEmail().withMessage('El usuario debe tener un correo valido'),
+        .isLength({min: 2, max: 40}).isEmail().withMessage('El usuario debe tener un correo valido'),
     body('password')
         .isLength({min: 8, max: 20}).withMessage('La contraseña debe ser de entre 8 y 20 caracteres')
 ]
