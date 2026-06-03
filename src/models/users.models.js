1a24a3570adab5fba73bb0c2025ceaaea3bd8878
zaxwtf <zaxwtf06@gmail.com>
2026-06-03 15:45:26 +0200

    refactor: agregados unique y trim a tipado de email

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -25,6 +25,8 @@ const userSchema = new mongoose.Schema(
         email: {
             type: String,
             required: true,
+            unique: true,
+            trim: true,
         },
         password: {
             type: String,
