9fd3e5ca9b2922acd661636c58e35a97ee5133c9
zaxwtf <zaxwtf06@gmail.com>
2026-06-03 18:51:29 +0200

    refactor: cambiada información de validación de juegos y usuario a sus propios archivos

--- src/middlewares/validar.js
+++ src/middlewares/validar.js
@@ -1,40 +1,5 @@
 const { body, validationResult } = require('express-validator')
 
-const genders = [
-    "lucha",
-    "rpg",
-    "arcade",
-    "plataformas",
-    "disparos",
-    "simulacion",
-    "estrategia",
-    "a_rpg"
-]
-
-const validarJuego = [
-    body('nombre')
-        .notEmpty().withMessage('El juego debe tener nombre'),
-    body('precio')
-        .notEmpty().withMessage('El juego debe tener precio')
-        .isNumeric().withMessage('El precio debe ser un número'),
-    body('genero')
-        .notEmpty().withMessage('El juego debe tener género')
-        .custom(genero => genders.includes(genero))
-        .withMessage('El juego debe tener un género válido')
-]
-
-const validarUser = [
-    body('nombre')
-        .notEmpty().withMessage('El usuario debe tener nombre'),
-    body('apellido1')
-        .notEmpty().withMessage('El usuario debe tener por lo menos el primer apellido'),
-    body('userName')
-        .notEmpty().withMessage('El usuario debe tener nombre de usuario'),
-    body('email')
-        .notEmpty().isEmail().withMessage('El usuario debe tener un correo valido'),
-    body('password')
-        .notEmpty().isLength({min: 8, max: 20}).withMessage('La contraseña debe ser de entre 8 y 20 caracteres')
-]
 
 function validate(req, res, next) {
     const errors = validationResult(req)
@@ -45,7 +10,5 @@ function validate(req, res, next) {
 }
 
 module.exports = {
-    validarJuego,
-    validarUser,
     validate
 }
