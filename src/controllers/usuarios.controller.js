a73dbf9ff309252ddbe4225a03d40faf3f23d4b4
zaxwtf <zaxwtf06@gmail.com>
2026-06-05 16:38:18 +0200

    fix: recolocado orden de funciones para que se registre el usuario correctamente y devuelva el mensaje esperado en vez de "este email ya está registrado"

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -39,13 +39,17 @@ async function crearUser(req, res){
     const userNameUnico = `${userName}#${Math.trunc(Math.random() * 1000)}`
     try{
         const passwordHashed = await bcrypt.hash(password, 10)
+        const emailExiste = await modelUsers.validateEmail(email)
+        if (emailExiste){
+            console.log(emailExiste)
+            return res.status(409).json("Este email ya está registrado")
+        }
         const newUser = await modelUsers.crearUser({nombre, apellido1, apellido2, userName, userNameUnico, email, password: passwordHashed})
+        
         if (!newUser){
             return res.status(404).json("Error del usuario")
         }
-        if (modelUsers.validateEmail(email)){
-            return res.status(409).json("Este email ya está registrado")
-        }
+        
         
         return res.status(201).json({
             id: newUser._id,
@@ -63,8 +67,39 @@ async function crearUser(req, res){
 }
 
 
+async function loginUser(req, res) {
+    try{
+        const {email, password} = req.body
+        const user = await modelUsers.getUsersByEmail(req.body.email)
+
+        if (!user){
+            return res.status(401).json({"error": "Credenciales incorrectas"})
+        }
+
+        const match = await bcrypt.compare(password, user.password)
+
+        if (!match){
+            return res.status(401).json({"error": "Credenciales incorrectas"})
+        }
+
+        res.status(201).json({
+            message: "login correcto",
+            id: user._id,
+            email: user.email,
+        });
+    } catch(error){
+        console.error("error:", error)
+        res.status(500).json({"error": "Error en login"})
+    }
+    
+    
+
+}
+
+
 module.exports = {
     obtenerUserById,
     obtenerUserByEmail,
-    crearUser
+    crearUser,
+    loginUser
 }
