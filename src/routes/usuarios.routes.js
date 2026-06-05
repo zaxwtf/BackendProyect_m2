306072b5d5903cf8ccd7624f7ff91092696eca40
zaxwtf <zaxwtf06@gmail.com>
2026-06-05 16:39:06 +0200

    feat: agregada ruta para logear usuarios

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -2,7 +2,8 @@ const { Router } = require('express')
 const router = Router()
 const usuariosController = require('../controllers/usuarios.controller');
 const {validarUser} = require('../middlewares/validarUser');
-const { validate } = require('../middlewares/validar')
+const { validate } = require('../middlewares/validar');
+const loginRules = require('../middlewares/validarLogin');
 
 
 //Rutas usuarios
@@ -10,5 +11,6 @@ const { validate } = require('../middlewares/validar')
 router.post('/crear', validarUser, validate, usuariosController.crearUser);
 router.get('/email', usuariosController.obtenerUserByEmail)
 router.get('/:id', usuariosController.obtenerUserById)
+router.post('/login', usuariosController.loginUser)
 
 module.exports = router;
