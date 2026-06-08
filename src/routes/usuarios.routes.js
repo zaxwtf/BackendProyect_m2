43062709737f3db74f45825669ea2400156803ab
zaxwtf <zaxwtf06@gmail.com>
2026-06-08 16:26:30 +0200

    feat: cambiadas rutas para que whoami sea un middleware

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -3,15 +3,16 @@ const router = Router()
 const usuariosController = require('../controllers/usuarios.controller');
 const {validarUser} = require('../middlewares/validarUser');
 const { validate } = require('../middlewares/validar');
+const { verifyToken } = require('../middlewares/verificarToken');
+
 
 
 //Rutas usuarios
 
 router.post('/crear', validarUser, validate, usuariosController.crearUser);
 router.get('/email', usuariosController.obtenerUserByEmail)
-router.get('/whoami', usuariosController.whoAmI)
 router.get('/:id', usuariosController.obtenerUserById)
-router.post('/login', usuariosController.loginUser)
+router.post('/login', verifyToken, usuariosController.loginUser)
 
 
 module.exports = router;
