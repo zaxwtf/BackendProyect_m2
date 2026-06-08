5566305abeb65512740cab2bd8701e1dedf4c23d
zaxwtf <zaxwtf06@gmail.com>
2026-06-08 18:39:52 +0200

    feat: autenticado que el header tiene que empezar por bearer y creado respuesta a error TokenExpiredError

--- src/middlewares/verificarToken.js
+++ src/middlewares/verificarToken.js
@@ -4,8 +4,8 @@ function verifyToken(req, res, next) {
     try{
         const authHeader = req.headers.authorization;
 
-        if (!authHeader){
-            return res.status(401).json({message: "No hay token"})
+        if (!authHeader || !authHeader.startsWith("Bearer ")){
+            return res.status(401).json({message: "No hay token o el formato es incorrecto"})
         }
 
         const token = authHeader.split(" ")[1]
@@ -16,6 +16,9 @@ function verifyToken(req, res, next) {
 
         next()
     }catch(error){
+        if (error.name === "TokenExpiredError"){
+            return res.status(401).json({message: "Tu sesión ha caducado:", error})
+        }
         return res.status(500).json({message: "error interno del servidor", error})
     }
 }
