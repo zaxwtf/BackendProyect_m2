7f12f0849e8c6b469cc50af0d1fd1b88009fc4b0
zaxwtf <zaxwtf06@gmail.com>
2026-06-08 16:28:20 +0200

    feat: creado middleware para verificar token

---
+++ src/middlewares/verificarToken.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken')
+
+function verifyToken(req, res, next) {
+    try{
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader){
+            return res.status(401).json({message: "No hay token"})
+        }
+
+        const token = authHeader.split(" ")[1]
+
+        const data = jwt.verify(token, process.env.JWT_SECRET)
+
+        return req.body = data
+
+        next()
+    }catch(error){
+        return res.status(500).json({message: "error interno del servidor", error})
+    }
+}
+
+
+module.exports = {
+    verifyToken
+}
