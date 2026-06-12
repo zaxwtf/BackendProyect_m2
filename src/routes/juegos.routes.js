660ff04b1685c5fd6fd64cb7b4d69853ada07352
zaxwtf <zaxwtf06@gmail.com>
2026-06-12 19:05:23 +0200

    feat: agregado verifyToken a la creacion de juego

--- src/routes/juegos.routes.js
+++ src/routes/juegos.routes.js
@@ -3,12 +3,13 @@ const router = Router();
 import * as juegosController from "../controllers/juegos.controller.js";
 import validarJuego from "../middlewares/validarJuego.js";
 import validate from "../middlewares/validar.js";
+import verifyToken from "../middlewares/verificarToken.js";
 
 
 //Definimos rutas de get y post y enlazamos a la función del controller
 router.get('/', juegosController.obtenerJuegos);
 router.get('/:id', juegosController.obtenerJuegoID);
-router.post('/crear', validarJuego, validate, juegosController.crearJuego);
+router.post('/crear', validarJuego, validate, verifyToken, juegosController.crearJuego);
 router.delete('/borrar/:id', juegosController.borrarJuego)
 router.put('/cambiar/:id', juegosController.actualizarJuego);
 
